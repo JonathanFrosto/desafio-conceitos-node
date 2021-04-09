5bc70bc588071e9034173d9c875ed5332a431940
Jonathan Anthony <jonataanthony@gmail.com>
2021-04-09 00:04:41 -0300

    Desafio completo

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,72 @@ app.use(cors());
 
 const repositories = [];
 
+function validId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).send();
+  }
+
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repositorie = { id: uuid(), title, likes: 0, url, techs };
+
+  repositories.push(repositorie);
+  response.status(201).json(repositorie);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validId, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const index = repositories.findIndex(repositorie => repositorie.id == id);
+
+  if (index == -1) {
+    return response.status(404).send();
+  }
+
+  const repositorie = { ...repositories[index], title, url, techs };
+
+  repositories[index] = repositorie
+  response.json(repositorie);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validId, (request, response) => {
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repositorie => repositorie.id == id);
+
+  if (index == -1) {
+    return response.status(404).send();
+  }
+
+  repositories.splice(index, 1);
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validId, (request, response) => {
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repositorie => repositorie.id == id);
+
+  if (index == -1) {
+    return response.status(404).send();
+  }
+
+  const repositorie = repositories[index];
+  repositorie.likes = repositorie.likes + 1;
+
+  repositories[index] = repositorie
+  response.json(repositorie);
 });
 
 module.exports = app;
